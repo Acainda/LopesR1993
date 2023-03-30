714b3965907cecde9c08c51e07e13314e129ef87
TIAGO CAROLINO <6355267@formacao.iefp.pt>
2023-03-30 19:57:50 +0100

    teste

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Olá Turma DotNet 001");
+Console.WriteLine("Benfica CAMPEAO");
